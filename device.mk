fe38e790f970d6f11ec27c369b24d50c95a0ffd4
dianlujitao <dianlujitao@lineageos.org>
2018-12-24 12:04:48 +0800

    davinci: Support Alipay and WeChat fingerprint payment
     * IFAA manager is based on OnePlusOSS, but adapted for Xiaomi's mlipay
       interface.
    
    Change-Id: Ied17d6456561bb399e4dd4a868d57a48d2d51db2
    
    davinci: Add IIFAAService for Alipay fingerprint payment
    
      * The org.ifaa.android.manager.IIFAAService has been moved
        from the SoterService.apk to MIUI framework, at least
        since MIUI 9.4.26. Adding this interface will fix the
        SoterService crashing when launching Alipay.
    
    Change-Id: Ic97467eb0a8fe92b49e0edbedd56f1866c5fa01e
    
    davinci: org.ifaa.android.manager: switch to blueprint
    
    * otherwise rip boot jars
    
    Change-Id: Iea769fb4d3e07202b5477d2a86e40dd04170a053
    
    davinci: Add ifaa apis to unrestricted greylist
    
    Change-Id: If33a18edaf6083281265ce94fd143d72b4bb4fb8
    
    davinci: Make IFAAManager compile again
    
    Change-Id: Iae2d1004bd1b3c939b552f09ea994ae87d71d73e
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -200,6 +200,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.freeform_window_management.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
+
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service
