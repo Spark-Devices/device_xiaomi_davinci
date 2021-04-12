b02e099c29cf0c35e4c124303cb4b48519ca0542
Marco Rizzo <marco.rizzo.00@outlook.com>
2021-04-12 07:49:35 +0000

    davinci: init: upstream fingerprint to april patches
    Signed-off-by: Marco Rizzo <marco.rizzo.00@outlook.com>

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -105,7 +105,7 @@ void vendor_load_properties() {
 
     // SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    fingerprint = "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys";
+    fingerprint = "google/sunfish/sunfish:11/RQ2A.210405.005/7181113:user/release-keys";
 
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
