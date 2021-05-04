013bfe98dc66f8cbaa517e8be330b0a502e3267c
spkal01 <kalligeross@gmail.com>
2021-05-04 10:58:26 +0530

    Davinci: init upstream sunfish fp

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -105,7 +105,7 @@ void vendor_load_properties() {
 
     // SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    fingerprint = "google/sunfish/sunfish:11/RQ2A.210405.005/7181113:user/release-keys";
+    fingerprint = "google/sunfish/sunfish:11/RQ2A.210505.002/7246365:user/release-keys";
 
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
